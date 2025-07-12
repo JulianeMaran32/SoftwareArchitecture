f4a992e14db0fb90bd9922d0ba92f2ed2f950ab5
JulianeMaran32 <julianemaran@gmail.com>
2025-07-12 19:59:03 -0300

    chore: projeto base criado com Spring Boot

---
+++ stock-event-sourcing/src/main/java/com/juhmaran/stockeventsourcing/StockEventSourcingApplication.java
@@ -0,0 +1,13 @@
+package com.juhmaran.stockeventsourcing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockEventSourcingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockEventSourcingApplication.class, args);
+	}
+
+}
