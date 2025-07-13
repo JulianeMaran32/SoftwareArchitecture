96b5426dc550514fa7abdc1e4ba2f18b0540177d
JulianeMaran32 <julianemaran@gmail.com>
2025-07-13 11:26:39 -0300

    feat(setup): Configurações base do projeto

--- stockeventsourcing/src/main/java/com/juhmaran/stockeventsourcing/StockeventsourcingApplication.java
+++ stockeventsourcing/src/main/java/com/juhmaran/stockeventsourcing/StockeventsourcingApplication.java
@@ -2,12 +2,14 @@ package com.juhmaran.stockeventsourcing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // Habilita a auditoria para timestamps automáticos
 public class StockeventsourcingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StockeventsourcingApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(StockeventsourcingApplication.class, args);
+  }
 
 }
