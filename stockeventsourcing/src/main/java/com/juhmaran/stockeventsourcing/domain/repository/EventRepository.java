a5ce5c0cb496a38fa9c3134cc78407bf95bcb370
JulianeMaran32 <julianemaran@gmail.com>
2025-07-14 13:57:01 -0300

    feat: implement domain model and event store

--- stockeventsourcing/src/main/java/com/juhmaran/stockeventsourcing/domain/repository/EventRepository.java
+++ stockeventsourcing/src/main/java/com/juhmaran/stockeventsourcing/domain/repository/EventRepository.java
@@ -1,8 +1,19 @@
 package com.juhmaran.stockeventsourcing.domain.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
 /**
  * Created by Juliane Maran
+ *
  * @since 13/07/2025
  */
-public interface EventRepository {
+@Repository
+public interface EventRepository extends JpaRepository<EventStore, UUID> {
+
+  List<EventStore> findByAggregateIdOrderByTimestampAsc(String aggregateId);
+
 }
--- stockeventsourcing/src/main/java/com/juhmaran/stockeventsourcing/domain/repository/EventStore.java
+++ stockeventsourcing/src/main/java/com/juhmaran/stockeventsourcing/domain/repository/EventStore.java
@@ -1,11 +1,40 @@
 package com.juhmaran.stockeventsourcing.domain.repository;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 /**
  * Created by Juliane Maran
+ *
  * @since 13/07/2025
  */
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "event_store")
 public class EventStore {
 
-    // TODO: Implementar funcionalidades da classe EventStore
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @Column(nullable = false)
+  private LocalDateTime timestamp;
+
+  @Column(nullable = false)
+  private String aggregateId;
+
+  @Column(nullable = false)
+  private String eventType;
+
+  @Lob
+  @Column(nullable = false, columnDefinition = "CLOB")
+  private String eventData;
 
 }
